676770827450c443cc9438aa6960ce343f9f033d
Sakshi <sakshi200201@gmail.com>
2025-09-10 17:33:10 +0530

    Variables

---
+++ basics/01_variables.js
@@ -0,0 +1,31 @@
+
+//variables are containers for data
+// Variable names are case sensitive;
+
+                //Variable Rules
+//  Variable names are case sensitive;“a” & “A” is different.
+// Only letters, digits, underscore( _ ) and $ is allowed. (not even space)
+// Only a letter, underscore( _ ) or $ should be 1st character.
+// Reserved words cannot be variable names.
+
+
+// let, const & var
+// var : Variable can be re-declared & updated. A global scope variable.
+// let : Variable cannot be re-declared but can be updated. A block scope variable.
+// const : Variable cannot be re-declared or updated. A block scope variable.
+
+
+const accountId = 6376328
+let accountEmail = "sakshi@google.com"
+var accountPassword = "2344"
+accountCity = "jaipur"
+let accountState;
+
+accountEmail = "sg@gmail.com"
+accountPassword = "578298"
+accountCity = "banaglore"
+
+console.table([accountEmail,accountPassword,accountCity]);
+
+
+
