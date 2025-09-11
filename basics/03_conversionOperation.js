c3010198df037bbb1348a401e08c75a08096c2ab
Sakshi <sakshi200201@gmail.com>
2025-09-11 17:12:33 +0530

    Conversion Operation

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,71 @@
+let score = "35abs"
+//console.log(typeof score);
+ 
+ let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "33" => 33
+//"33abs" => NaN
+// true => 1: false => 0
+// "und3efined" => NaN
+// Null => O
+
+   let isLoggegIn = "sakshi"
+   let boleanIsLoggedIn = Boolean(isLoggegIn)
+  // console.log(boleanIsLoggedIn);
+   
+   // 1 => true: 0=> false
+   // "" => false 
+   //"sakshi" => true
+
+   let someNumbver =  33
+   let stringSomeNumber = String(someNumbver)
+//    console.log(stringSomeNumber);
+//    console.log(typeof (stringSomeNumber));
+
+//  ***************** Operations ************** //
+ 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(typeof negValue);
+
+  let str1 = "hello"
+  let str2 = " sakshi" 
+  let str3 = str1 + str2
+// console.log(str3);
+//  console.log("1" + 2); //=> 12
+//   console.log(1 + "2"); //=> 12
+//   console.log("1" + 2 + 2); // => 122
+//   console.log(1 + 2 + "2"); // => 32
+
+console.log(null > 0); //false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined == 0);  //false
+console.log(undefined > 0);  //false
+console.log(undefined < 0 ); //false
+ // ==
+ console.log("2" == 2); // true
+ 
+
+// ===
+ console.log("2" === 2); //=> false: strict checking
+ 
+
+
+
+
+
+  
+  
+ 
+
+  
+
+
+
+   
