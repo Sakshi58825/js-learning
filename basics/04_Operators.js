516c6409899f9731c8fcf664d063cfb407667739
Sakshi <sakshi200201@gmail.com>
2025-09-11 22:28:20 +0530

    Operators

---
+++ basics/04_Operators.js
@@ -0,0 +1,74 @@
+//************* Arithmetic Operators*************//
+
+let a = 2
+let b = 6
+let c = a + b;
+//console.log(c);  // "+"" operator
+ 
+let d= 14
+let e = 7
+// console.log("d = ", d ,  "e = ", e);
+// console.log("d + e =", d + e);
+// console.log("d - e =", d - e);
+// console.log("d * e =", d * e);
+// console.log("d / e =", d / e);
+// console.log("d % e =", d % e); //modulus
+// console.log("d ** e =", d ** e); // exonential
+
+//***************Unary Operator *************//
+
+let f = 3;
+let g = 7;
+//console.log("f = ", f , "g = ", g);
+//f = f +1;
+// f++;  =>increment operator
+// g--; => Decrement Operator
+// console.log("f++", f++);//=> post increment
+// console.log(f);
+// console.log("++g", ++g); //=> pre increment
+// console.log(g);
+
+// **************************Assignment Opreators***********************//
+let value = 5 ; // Assign operator
+ value +=1 // value = value + 1
+ //console.log(value);
+  
+ let value2 = 7;
+ value2 -= 4; // value2 = value2 - 4 => 7-4=3
+//console.log(value2);
+
+ // *************************Comparison Operators ***************************//
+
+let i = 4;
+let j = 4;
+let k = "4";
+// console.log("4 == 4 ", i==j); // True
+// console.log("4 != 4 ", i!=j); //false
+// console.log("4 === 4", i===k); //false => strict operator
+// console.log("4 !== 4", i!==k); // true
+// console.log("4 > 4 ", i>j); //false
+// console.log("4 >= 4 ", i>=j); //true
+// console.log("4 <= 4 ", i<=j); //true 
+  
+// ******************Logical Operators**************//
+let l = 4;
+let m = 7;
+ let cond1 = l < m;
+ let cond2 = l===6;
+console.log(cond1 && cond2); //AND 
+console.log(cond1 || cond2); // OR
+console.log (!(l < m )); // Logical NOT
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
